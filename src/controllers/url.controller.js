795561fcfe9abf557f0b5b32b1fedfb377ec8ec6
Parth Garg <parth.garg6106@gmail.com>
2025-03-23 19:30:42 +0530

    Add a validator and updated the controller

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -2,16 +2,15 @@ import { Url } from "../models/url.model.js";
 import { asyncHandler } from "../utlities/asyncHandler.js";
 import { ApiError } from "../utlities/ApiError.js";
 import { ApiResponse } from "../utlities/ApiResponse.js";
+import validator from 'validator';
 
 function generateURL()
 {
-    console.log('URL shortening start')
     const text = 'abcdefghijklmnopqrstuvwxyz';
     const utext = 'ABCDEFGHIJKLMONPQRSTUVWXYZ';
     const number = '1234567890';
     const values = text+utext+number;
     let createURL;
-    console.log(values.length);
     for(let i=0;i<5;i++)
     {
         if(!createURL)
@@ -23,23 +22,22 @@ function generateURL()
             createURL += values[Math.floor(Math.random()*values.length)];
         }
     }
-    console.log(createURL);
     return createURL;
 }
 
 export const createShortURL = asyncHandler(async(req,res)=>
     {
-        console.log('Start')
         const {url} = req.body;
 
-        console.log(url)
         if(!url)  
         {
             throw new ApiError(400,'Url is missing');
         }
 
+        const isValidURL = validator.isURL(url);
+        if(!isValidURL) throw new ApiError(400,'Invalid URL entered')
+
         const existingUrl = await Url.findOne({url});
-        console.log(existingUrl);
         if(existingUrl)
         {
             throw new ApiError(403,'Url is already present');
@@ -47,7 +45,6 @@ export const createShortURL = asyncHandler(async(req,res)=>
 
         const shortUrl = generateURL();
 
-        console.log('ShortURL->',shortUrl);
 
         if(!shortUrl)
         {
@@ -55,9 +52,7 @@ export const createShortURL = asyncHandler(async(req,res)=>
         }
 
         try {
-            console.log('Url is being getting started to enter into db')
             const newUrl = await Url.create({url,shortUrl});
-            console.log(newUrl);
             if(!newUrl)
             {
                 throw new ApiError(400,'An error occured');
@@ -80,17 +75,13 @@ export const createShortURL = asyncHandler(async(req,res)=>
 export const getOriginalURL = asyncHandler(async(req,res)=>
     {
 
-        console.log('The original url is being started being fetched')
         const shortId = req.params.shortId;
-        console.log(shortId);
         if(!shortId)
         {
             throw new ApiError('Kindly enter the short url');
         }
 
-        console.log('Search for the original url has been started')
         const originalURL = await Url.findOne({shortUrl:shortId});
-        console.log('Check for the existence')
         if(!originalURL)
         {
             throw new ApiError(404,'The url does not exsist');
@@ -125,7 +116,6 @@ export const updateURL = asyncHandler(async(req,res)=>{
 })
 
 export const deleteURL = asyncHandler(async(req,res)=>{
-    console.log('Started')
     const shortId = req.params.shortId;
     console.log(shortId);
     if(!shortId?.trim())
